63fc5e0a0a13c34c123cac7320c2de7a567dd57b
Callum Mitchell <mitccall@gmail.com>
2022-04-17 05:37:42 -0400

    Create placeholder board/tile assets and visual example of a valid board

--- MagicWordsUnity/Assets/Scripts/Board/LetterTile.cs
+++ MagicWordsUnity/Assets/Scripts/Board/LetterTile.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelBoard : MonoBehaviour
+public class LetterTile : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject m_highlightObj, m_textObj, m_silhouetteTextObj;
+
     // Start is called before the first frame update
     void Start()
     {
